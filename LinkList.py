5ea393fa7029414000a9ae44b1a359124394946b
Kumar, Chandan <chandank@qti.qualcomm.com>
2020-03-21 17:36:32 +0530

    v1.2

--- LinkList.py
+++ LinkList.py
@@ -19,9 +19,33 @@ class Node(object):
 
 class LinkList(object):
     
-
+    def __init__(self):
+        '''
+        Return head
+        '''
+        head = Node()
+        return head
+    def insert(self,data,pos=None):
+        if pos is None:
+            '''
+            Insert at end
+            '''
+            temp = self.head
+            while temp.get_next() != None:
+                temp = temp.get_next()
+            t = Node()
+            t.set_data(data)
+            temp.set_next(t)
+        else:
+            if pos.upper() == "START":
+                temp = Node()
+                temp.set_data(data)
+                temp.set_next(self.head.get_next())
+                self.head.set_next(temp)
+        
 def main():
     
 
 if __name__ == '__main__':
     main()
+    print("Hello")
